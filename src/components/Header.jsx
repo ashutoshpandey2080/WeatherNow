1928655b5f69f4b5d78e70b5086e0b729433146b
Ashutosh Pandey <118986310+ashutoshpandey2080@users.noreply.github.com>
2024-11-10 12:38:02 +0530

    Update Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="p-4 text-center">
+      <h1 className="text-4xl font-bold text-gradient animate-gradient font-roboto">Weather Now</h1>
+    </header>
+  );
+};
+
+export default Header;
